cca311394ef662e5f48c89a170d1213288a10bdf
duongtruongvu21 <duongtruongvu21@gmail.com>
2022-11-02 21:10:01 +0700

    test

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -4,8 +4,12 @@ namespace PBL5BE.API.Data.Entities
 {
     public class User
     {
+
+
         public int ID { get; set; }
-        [Required][EmailAddress][MaxLength(64)]
+        [Required]
+        [EmailAddress]
+        [MaxLength(64)]
         public string Email { get; set; }
 
         public byte[] PasswordHashed { get; set; }
